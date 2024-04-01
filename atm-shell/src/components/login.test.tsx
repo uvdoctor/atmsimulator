9c7cf4967c07204b9dc188dc0f8a6ac45102bfba
Umang V Doctor <emailumangdoctor@gmail.com>
2024-04-01 22:14:04 +0530

    sample jest test for login

--- atm-shell/src/components/login.test.tsx
+++ atm-shell/src/components/login.test.tsx
@@ -1,10 +1,11 @@
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
+import React from 'react'
 import Login from './login'
  
 describe('Login', () => {
   it('check for login text', () => {
     render(<Login />);
-    expect(screen.getByText("Login"));
+    expect(screen.getByText("Login")).toBeInTheDocument();
   })
 })
