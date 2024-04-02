a3d07ec7567a3273884b72f9dd1ad60bbc72cfce
Umang V Doctor <emailumangdoctor@gmail.com>
2024-04-02 09:42:08 +0530

    Updated analytics to only work in prod

--- atm-shell/src/components/login.tsx
+++ atm-shell/src/components/login.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import { sendGAEvent } from '@next/third-parties/google'
 
 export default function Login() {
-    return <div>Login</div>
+    return (
+        <div>
+            <button onClick={() => sendGAEvent({ event: 'loginClicked', value: 'login' })}>
+                Login
+            </button>
+        </div>
+    );
 }
--- atm-shell/src/pages/_document.tsx
+++ atm-shell/src/pages/_document.tsx
@@ -1,22 +1,9 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { revalidate } from '@module-federation/nextjs-mf/utils';
-
-//@ts-ignore
-export const getStaticProps = (async (context: any) => {
-  if (context?.pathname && !context?.pathname?.endsWith('_error')) {
-    await revalidate().then((shouldUpdate) => {
-      if (shouldUpdate) {
-        console.log('Hot Module Replacement (HMR) activated', shouldUpdate);
-      }
-    });
-  }
-  return {props: {}, revalidate: 10};
-});
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head/>
       <body>
         <Main />
         <NextScript />
