531a2db6810fa5a8d7986c02d4d2e7e0340ef695
Umang V Doctor <emailumangdoctor@gmail.com>
2024-04-02 10:13:39 +0530

    Implemented monitoring using Sentry for shell

--- atm-shell/next.config.mjs
+++ atm-shell/next.config.mjs
@@ -1,3 +1,4 @@
+import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
 import withNextIntl from 'next-intl/plugin';
 import { NextFederationPlugin } from '@module-federation/nextjs-mf';
@@ -35,4 +36,38 @@ const nextConfig = {
 /*const nextWithIntl = withNextIntl('./i18n.tsx')(nextConfig);
 
 export default nextWithIntl;*/
-export default nextConfig;
+export default withSentryConfig(nextConfig, {
+// For all available options, see:
+// https://github.com/getsentry/sentry-webpack-plugin#options
+
+// Suppresses source map uploading logs during build
+silent: true,
+org: "moneo-60",
+project: "atm-simulator",
+}, {
+// For all available options, see:
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+// Upload a larger set of source maps for prettier stack traces (increases build time)
+widenClientFileUpload: true,
+
+// Transpiles SDK to be compatible with IE11 (increases bundle size)
+transpileClientSDK: true,
+
+// Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers. (increases server load)
+// Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+// side errors will fail.
+tunnelRoute: "/monitoring",
+
+// Hides source maps from generated client bundles
+hideSourceMaps: true,
+
+// Automatically tree-shake Sentry logger statements to reduce bundle size
+disableLogger: true,
+
+// Enables automatic instrumentation of Vercel Cron Monitors.
+// See the following for more information:
+// https://docs.sentry.io/product/crons/
+// https://vercel.com/docs/cron-jobs
+automaticVercelMonitors: true,
+});
